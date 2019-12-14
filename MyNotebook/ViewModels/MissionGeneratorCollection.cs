ea7634b3f60c83810291d1a5e971dc4a84a5e618
diademoff <diademoff@yandex.ru>
2019-12-14 08:53:09 +0300

    add mission 18

--- MyNotebook/ViewModels/MissionGeneratorCollection.cs
+++ MyNotebook/ViewModels/MissionGeneratorCollection.cs
@@ -22,7 +22,8 @@ namespace MyNotebook.ViewModels
             new Mission14(),
             new Mission15(),
             new Mission16(),
-            new Mission17()
+            new Mission17(),
+            new Mission18()
         };
     }
 }
